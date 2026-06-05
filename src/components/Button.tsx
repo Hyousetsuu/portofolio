d6cd3a6e742a809b5b2b4892d01629ede62bae31
Hyousetsuu <SWAT2209094@gmail.com>
2026-06-06 00:02:18 +0700

    fix: remove unused import to fix vercel build

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { cn } from '../utils/cn';
-import type { HTMLMotionProps } from 'framer-motion';
+
 import { motion } from 'framer-motion';
 
 export interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
